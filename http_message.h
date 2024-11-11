45104c8c9e9ef76c04e3cc321867524425b3ee79
Amal Suresh <amalsuresh@csu.fullerton.edu>
2024-11-10 23:10:53 -0800

    unfinished code. Still need stats, static, and calc.

---
+++ http_message.h
@@ -0,0 +1,21 @@
+#ifndef HTTP_MESSAGE_H
+#define HTTP_MESSAGE_H
+
+typedef struct message {
+    char* method;
+    char* path;
+    char* http_version;
+    int body_length;
+    char *headers;
+} http_client_message_t;
+
+typedef enum {
+    BAD_REQUEST,
+    CLOSED_CONNECTION,
+    MESSAGE
+} http_read_result_t;
+
+void read_http_client_message(int client_socket, http_client_message_t** msg, http_read_result_t* result);
+void http_client_message_free(http_client_message_t* msg);
+//bool is_complete_http_message(char* buffer);
+#endif
